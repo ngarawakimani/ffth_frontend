f8248816f682810569f344d01c570e6564156517
ngarawakimani <dancankimani70@gmail.com>
2019-07-08 12:21:30 +0300

    ui set up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ChildrenComponent } from './children/children.component';
+import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { CrisisComponent } from './crisis/crisis.component';
+import { SponsorshipComponent } from './sponsorship/sponsorship.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChildrenComponent,
+    LoginComponent,
+    RegistrationComponent,
+    CrisisComponent,
+    SponsorshipComponent
   ],
   imports: [
     BrowserModule
